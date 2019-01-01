8c79d8b53e9c7b832417113b48fba0447a481e44
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2019-01-01 15:36:01 +0700

    cleanup

--- src/Main.elm
+++ src/Main.elm
@@ -33,46 +33,39 @@ update msg model =
 
 -- VIEW
 
-getPointsHtml = 
-    getPoints 480 360 -240 180 0 0
-    |> List.indexedMap 
-        (\rowNo row -> 
-            row |> List.indexedMap (\colNo point ->
-                rect 
-                    [ x (String.fromInt colNo)
-                    , y (String.fromInt rowNo)
-                    , width "1"
-                    , height "1"
-                    , fill <| if point.on then "green" else "none"
-                    ]
-                    []
+getPointsHtml : Int -> Int -> List (Html Msg)
+getPointsHtml w h  = 
+    let 
+        halfHeight = (toFloat h) / 2
+        halfWidth = (toFloat w) / 2
+    in
+        getPoints w h -halfWidth halfHeight halfWidth -halfHeight
+        |> List.indexedMap 
+            (\rowNo row -> 
+                row |> List.indexedMap (\colNo point ->
+                    rect 
+                        [ x (String.fromInt colNo)
+                        , y (String.fromInt rowNo)
+                        , width "1"
+                        , height "1"
+                        , fill <| if point.on then "green" else "none"
+                        ]
+                        []
+                )
             )
-        )
-    |> List.concat
+        |> List.concat
 
 view : Model -> Html Msg
 view model =
-  svg
-    [ width "480"
-    , height "360"
-    , viewBox "0 0 480 360"
-    ]
-    getPointsHtml
-
-        
-    -- [ rect
-    --     [ x "10"
-    --     , y "10"
-    --     , width "100"
-    --     , height "100"
-    --     , rx "15"
-    --     , ry "15"
-    --     ]
-    --     []
-    -- , circle
-    --     [ cx "50"
-    --     , cy "50"
-    --     , r "50"
-    --     ]
-    --     []
-    -- ]
+    let 
+        canvasWidth = 480
+        canvasHeight = 360
+        stringCanvasWidth = (String.fromInt canvasWidth)
+        stringCanvasHeight = (String.fromInt canvasHeight)
+    in
+        svg
+            [ width stringCanvasWidth
+            , height stringCanvasHeight
+            , viewBox ("0 0 " ++ stringCanvasWidth ++ stringCanvasHeight)
+            ]
+            (getPointsHtml canvasWidth canvasHeight)
